7ad665a9192e26cb22b78195940d094d1fc268ad
Patrick Wells <pat.taylorwells@gmail.com>
2020-08-27 16:03:25 -0400

    final

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,21 +5,31 @@ var dbConnect = require("../db/db.json");
 
 module.exports = function (app){
     app.get("/api/notes", (req,res) => {
-        
-        
-        res.json(dbConnect)
+        console.log("in here");
+        res.json(dbConnect);
     });
 
     app.post("/api/notes", (req,res) => {
-        
-        
-        res.json(dbConnect)
+        console.log("in post");
+        let newNote = req.body;
+        newNote.id = Math.round(Math.random()*10);
+        dbConnect.push(newNote);
+        fs.writeFile("../db/db.json", JSON.stringify(dbConnect),
+        function(err){
+            console.log(err);
+        })
+        res.json(dbConnect);
     })
 
     app.delete("/api/notes/:id", (req,res) => {
-
-        
-    })
-
-
+        console.log("in delete");
+        let noteNum = req.params.id;
+        dbConnect = dbConnect.filter( note => note.id != noteNum);
+        fs.writeFile("../db/db.json", JSON.stringify(dbConnect),
+        function(err) {
+            console.log("Delete Success");
+        });
+        res.json(dbConnect);
+    });
 };
+
